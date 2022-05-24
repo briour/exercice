978c13e09e7317f96ef3f47a09770dea70a4f9f9
briour <68699051+briour@users.noreply.github.com>
2022-05-24 17:40:52 +0200

    Update and rename README.md to Exercice.R

---
+++ Exercice.R
@@ -0,0 +1,12 @@
+# Soit une distance "dist", en nombre de pas, représentée par un entier positif
+# Vous pouvez avancer de 1, 2, ou 3 pas à chaque fois
+# Combien y a-t-il de manières différentes de couvrir la distance ?
+
+# Exemple :
+
+dist = 3
+result = 4
+# 1 + 1 + 1
+# 1 + 2
+# 2 + 1
+# 3
